541072f17af61098fa53aa4ff1d26e0fb4cb7c43
wondermach <davide.torre@gmail.com>
2022-09-28 21:37:20 +0200

    Added PUBLIC_IP env variable

--- images/flare_observation/start_observation_node.sh
+++ images/flare_observation/start_observation_node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting flare observation node"
-/data/flare/go-flare/avalanchego/build/avalanchego --network-id=flare --http-host=\
+/data/flare/go-flare/avalanchego/build/avalanchego --network-id=flare --http-host= \
   --bootstrap-ips="$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeIP" }' -H 'content-type:application/json;' https://flare.flare.network/ext/info | jq -r ".result.ip")"  \
   --bootstrap-ids="$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeID" }' -H 'content-type:application/json;' https://flare.flare.network/ext/info | jq -r ".result.nodeID")" \
-  --db-dir=/data/flare-data/ --log-dir=/data/flare-logs/ 2>&1
+  --public-ip=${PUBLIC_IP} --db-dir=/data/flare-data/ --log-dir=/data/flare-logs/ 2>&1
