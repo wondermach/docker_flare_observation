79233f8090bca636c43e1b7322551ddaac36478f
wondermach <davide.torre@gmail.com>
2023-09-19 08:54:41 +0200

    Aggiornamento per la versione con validatore

--- images/flare_observation/start_observation_node.sh
+++ images/flare_observation/start_observation_node.sh
@@ -3,4 +3,4 @@ echo "Starting flare observation node"
 /data/flare/go-flare/avalanchego/build/avalanchego --network-id=flare --http-host= \
   --bootstrap-ips="$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeIP" }' -H 'content-type:application/json;' https://flare.flare.network/ext/info | jq -r ".result.ip")"  \
   --bootstrap-ids="$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeID" }' -H 'content-type:application/json;' https://flare.flare.network/ext/info | jq -r ".result.nodeID")" \
-  --public-ip=${PUBLIC_IP} --db-dir=/data/flare-data/ --log-dir=/data/flare-logs/ 2>&1
+  --public-ip=${PUBLIC_IP} --data-dir=/data/flare-data/  --config-file /data/flare/go-flare/config/localflare/C/config.wonderftso.json 2>&1
